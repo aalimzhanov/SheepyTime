bb73175dcad3104b9d829107e4c8e9bbd5bad6ff
Adil <aalimzhanov@mta.ca>
2024-03-12 01:06:35 -0300

    view comments added

--- views/CardView.java
+++ views/CardView.java
@@ -2,7 +2,18 @@ package views;
 
 import models.Card;
 
+/**
+ * This class represents a CardView, which is responsible for displaying information of a Card.
+ * 
+ * @author Adil Alimzhanov, Tan Karageldi, Tolga Cohce, Derrick Ansah
+ */
 public class CardView {
+    
+    /**
+     * Displays the information of a Card.
+     * 
+     * @param model the Card object containing the information to be displayed
+     */
     public void displayInformation(Card model){
         System.out.println(model.getInformation() );
     }
--- views/PlayerView.java
+++ views/PlayerView.java
@@ -1,8 +1,21 @@
 package views;
 import models.Player;
 
+/**
+ * This class represents the view for a player in the SheepyTime game.
+ * It is responsible for updating the view with the player's information and displaying any errors that occur.
+ *
+ * @author Adil Alimzhanov, Tan Karageldi, Tolga Cohce, Derrick Ansah
+ */
 public class PlayerView {
     
+    /**
+     * Updates the view with the player's information.
+     * Prints the player's name, the color of their sheep, whether their sheep is scared or not,
+     * the information of their first and second card (if available), and the number of zzzs they have.
+     *
+     * @param player the player whose information needs to be displayed
+     */
     public void updateView(Player player) {
         System.out.println("Player: " + player.getName());
         System.out.println("Sheep: " + player.getSheep().getColor());
@@ -16,6 +29,11 @@ public class PlayerView {
         System.out.println("Player has " + player.getNumOfZzzs() + " zzzs");
     }
 
+    /**
+     * Displays an error message.
+     *
+     * @param message the error message to be displayed
+     */
     public void showError(String message) {
         System.err.println("Error: " + message);
     }
