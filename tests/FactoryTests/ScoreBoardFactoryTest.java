9d0f0ee18768eddf395e46b6ebe76b698ce4b172
Adil <aalimzhanov@mta.ca>
2024-03-12 23:52:48 -0300

    Create ScoreBoardFactoryTest.java

---
+++ tests/FactoryTests/ScoreBoardFactoryTest.java
@@ -0,0 +1,28 @@
+package tests.FactoryTests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import factories.ScoreBoardFactory;
+import controllers.PlayerController;
+import models.Player;
+import views.PlayerView;
+import controllers.ScoreBoardController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScoreBoardFactoryTest {
+
+    @Test
+    public void testInitializeScoreBoard_ReturnsScoreBoardController() {
+        List<PlayerController> playerControllers = new ArrayList<>();
+        Player player = new Player("Adil", "blue");
+        playerControllers.add(new PlayerController(player, new PlayerView()));
+        ScoreBoardController scoreBoardController = ScoreBoardFactory.initializeScoreBoard(playerControllers);
+        assertNotNull(scoreBoardController);
+        scoreBoardController.gainWinks(player, 2);
+        assertEquals(2, player.getWinks());
+    }
+
+}
