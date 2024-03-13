6c05418a1c7cceff7c058dd005f29e893051ca9e
Adil <aalimzhanov@mta.ca>
2024-03-13 00:08:00 -0300

    Update CatchZzzCardTest.java
    Full testing is not possible due to Scanner input

--- tests/CardTests/CatchZzzCardTest.java
+++ tests/CardTests/CatchZzzCardTest.java
@@ -1,9 +1,6 @@
 package tests.CardTests;
 
 import models.cards.CatchZzzCard;
-import models.GameBoard;
-import models.Player;
-import views.UserInput;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,28 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CatchZzzCardTest {
 
     private CatchZzzCard catchZzzCard;
-    private Player player;
-    private GameBoard gameBoard;
-    private UserInput input;
 
     @BeforeEach
     void setUp() {
-        int zzzs = 1;
-        catchZzzCard = new CatchZzzCard(zzzs);
-        player = new Player("tolga","blue"); 
-        gameBoard = new GameBoard(); 
+        catchZzzCard = new CatchZzzCard(1);
     }
 
     @Test
     void testGetInformation() {
         String expectedInformation = "Catch 1 zzz";
-        assertEquals(expectedInformation, catchZzzCard.getInformation(), "The information should be the expected information");
+        assertEquals(expectedInformation, catchZzzCard.getInformation(),
+                "The information should be the expected information");
     }
-
-    // @Test
-    // void testExecuteAction() {
-    //     assertDoesNotThrow(() -> catchZzzCard.executeAction(player, gameBoard, input));
-    // }
 
     @Test
     void testIsNightmare() {
